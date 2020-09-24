6f6b9ba65ab56acccbd6ee8d322078462f77e9f0
Samridhi Kansal <kansal.samridhi@gmail.com>
2020-09-24 15:03:23 +0530

    form latest changes

--- src/components/invoice/invoiceForm/invoice/generateInvoice.js
+++ src/components/invoice/invoiceForm/invoice/generateInvoice.js
@@ -2,43 +2,77 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import uuid from "react-uuid";
+import "./generateinvoice.css";
 
 class GenerateInvoice extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      displayCat: false,
-      selectCat: false,
+      //   displayCat: false,
+      //   selectCat: false,
       itemCat: "",
       itemList: "",
+      item: {},
     };
   }
 
   render() {
-    console.log("cat is " + this.state.itemCat);
-
-    console.log();
-
+    console.log(this.props.itemsData);
+    console.log(
+      this.props.itemsData.filter(
+        (items) => items.category === this.state.itemCat
+      )
+    );
     return (
       <div>
         <h3>Generate Invoice </h3>
-        {/* value={this.state.value} onChange={this.handleChange} */}
-        <label>Select Category</label>
-        <select>
-          {this.props.itemsData.map((item) => {
-            return <option value={item.category}>{item.category}</option>;
-          })}
-        </select>
-        <label>Select Sub Category</label>
-        <select>
-          {this.props.itemsData.map((item) => {
-            return <option value={item.category}>{item.category}</option>;
-          })}
-        </select>
-        {this.props.itemsData.map((item) => {
-          return <p>{item.cat}</p>;
-        })}
-        <form>
+        <form className="form">
+          <label>Select Category</label>
+          <select
+            id="itemcat"
+            value={this.state.itemCat}
+            onChange={(e) =>
+              this.setState({ ...this.state, itemCat: e.target.value })
+            }
+          >
+            <option value=""> Choose an option </option>
+            {this.props.itemsData.map((item) => {
+              return (
+                <option key={uuid()} value={item.category}>
+                  {item.category}
+                </option>
+              );
+            })}
+          </select>
+          {/* Category sub-cat */}
+          <label id="itemlist">
+            Select Sub Category for {this.state.itemCat}
+          </label>
+          <select
+            id="itemlist"
+            value={this.state.item.name}
+            onChange={(e) =>
+              this.setState({
+                ...this.state,
+                item: { ...this.state.item, name: e.target.value },
+              })
+            }
+          >
+            <option value=""> Select an item from sub menu</option>
+            {/* {this.props.itemsData[0].itemList.map((item) => {
+              return <option> {item.item}</option>;
+            })} */}
+            {this.props.itemsData
+              .filter((item) => item.category == this.state.itemCat)
+              .map((item) => {
+                return item.itemList.map((itemlist) => {
+                  return <option> {itemlist.item}</option>;
+                });
+              })}
+          </select>
+        </form>
+
+        {/* <form>
           <label>Full Name</label>
           <input type="text"></input>
           <button
@@ -91,8 +125,8 @@ class GenerateInvoice extends React.Component {
                 // ))}
               }
             </div>
-          ) : null}
-        </form>
+          ) : null} */}
+        {/* </form> */}
         <Link to="/displayinvoice">
           {" "}
           <button> Display Invoice</button>{" "}
