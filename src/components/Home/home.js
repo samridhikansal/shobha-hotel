f60feccc16002f1d30bde925c42ac8d959b81e27
Samridhi Kansal <kansal.samridhi@gmail.com>
2020-09-24 13:49:21 +0530

    some updates

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = ({ message }) => {
   return (
     <div>
       <h1> {message}</h1>
+      <Link to="/generateinvoice">
+        <button> Generate Invoice</button>
+      </Link>
     </div>
   );
 };
